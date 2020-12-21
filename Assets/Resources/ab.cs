d99c434d342a2ad12902257da94e63a7b3d227d9
Devid Chhaya <chhayadevid1993@gmail.com>
2020-12-21 11:10:49 +0530

    Ball Sort Puzzle

---
+++ Assets/Resources/ab.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ab : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //var squarePos= new Vector2(squareBox.transform.position.x, squareBox.transform.position.y+2.25f); ;
+        //temp=Instantiate(outBox,squarePos,Quaternion.identity);
+
+        //Debug.Log(outBox.transform.position);
+        //for (i = 0; i < 4; i++)
+        //{
+        //    var curPos =new Vector2(squareBox.transform.GetChild(i).transform.position.x, squareBox.transform.GetChild(i).transform.position.y);
+        //    circleList2.Add(Instantiate(circleList[Random.Range(1,7)],curPos,Quaternion.identity));
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //var outPos = new Vector2(temp.transform.GetChild(5).transform.localPosition, temp.transform.position.y);
+        //Debug.Log("Out Box Position " +outPos+" Last Circle in List " +circleList2[listLength - 1].name);
+        ////circleList2[listLength - 1].transform.Translate(new Vector2(outPos.x, outPos.y));
+        //circleList2[listLength - 1].transform.Translate(new Vector2(outPos.x, outPos.y));
+
+        //outBox = circleList2[listLength - 1]; 
+        //Destroy(circleList2[listLength - 1]);
+
+    }
+}
---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,211 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    public GameObject squareBox;
+    GameObject abc;
+    public List<Material> materialList;
+    List<GameObject> tempList = new List<GameObject>();
+    int listLength, pos = -2, rePosTempListChildl;
+    public List<string> tagName;
+    string hitGameObjectname, rePosTemplist;
+    bool isRepos = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int randomBox = 3;
+        for (int i = 0; i < randomBox; i++)
+        {
+            tempList.Add(Instantiate(squareBox, new Vector2(transform.position.x + pos, transform.position.y), Quaternion.identity));
+            tempList[i].gameObject.name = "Square" + i;
+            listLength = Random.Range(2, 3);
+            for (int j = 0; j < listLength; j++)
+            {
+                int materialColor = Random.Range(0, 2);
+                tempList[i].transform.GetChild(j).gameObject.SetActive(true);
+                tempList[i].transform.GetChild(j).gameObject.GetComponent<SpriteRenderer>().material = materialList[materialColor];
+                tempList[i].transform.GetChild(j).gameObject.tag = tagName[materialColor];
+            }
+            pos = pos + 2;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
+
+        if (Input.GetMouseButtonDown(0) && hit.collider)
+        {
+            if (!isRepos)
+            {
+                hitGameObjectname = hit.collider.gameObject.name;
+                positionChange(hitGameObjectname);
+            }
+            else
+            {
+                reposChange(hit);
+            }
+        }
+    }
+    void positionChange(string i)
+    {
+        for (int j = 3; j >= 0; j--)
+        {
+            if (GameObject.Find(i).transform.GetChild(j).gameObject.activeInHierarchy)
+            {
+                Debug.Log(j);
+                if (j == 3)
+                {
+                    int p = j;
+                    if ((GameObject.Find(i).transform.GetChild(p).gameObject.tag == GameObject.Find(i).transform.GetChild(p - 1).gameObject.tag) && (GameObject.Find(i).transform.GetChild(p).gameObject.tag == GameObject.Find(i).transform.GetChild(p - 2).gameObject.tag) && (GameObject.Find(i).transform.GetChild(p).gameObject.tag == GameObject.Find(i).transform.GetChild(p - 3).gameObject.tag))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        rePosTemplist = i;
+                        rePosTempListChildl = j;
+                        GameObject.Find(i).transform.GetChild(4).gameObject.GetComponent<SpriteRenderer>().material = GameObject.Find(i).transform.GetChild(j).gameObject.GetComponent<SpriteRenderer>().material;
+                        GameObject.Find(i).transform.GetChild(4).gameObject.tag = GameObject.Find(i).transform.GetChild(j).gameObject.tag;
+                        GameObject.Find(i).transform.GetChild(4).gameObject.SetActive(true);
+                        GameObject.Find(rePosTemplist).transform.GetChild(rePosTempListChildl).gameObject.SetActive(false);
+                        isRepos = true;
+                        break;
+                    }
+                }
+                else
+                {
+                    rePosTemplist = i;
+                    rePosTempListChildl = j;
+                    GameObject.Find(i).transform.GetChild(4).gameObject.GetComponent<SpriteRenderer>().material = GameObject.Find(i).transform.GetChild(j).gameObject.GetComponent<SpriteRenderer>().material;
+                    GameObject.Find(i).transform.GetChild(4).gameObject.tag = GameObject.Find(i).transform.GetChild(j).gameObject.tag;
+                    GameObject.Find(i).transform.GetChild(4).gameObject.SetActive(true);
+                    GameObject.Find(rePosTemplist).transform.GetChild(rePosTempListChildl).gameObject.SetActive(false);
+                    isRepos = true;
+                    break;
+                }
+                
+            }
+        }
+    }
+
+    void reposChange(RaycastHit2D hit)
+    {
+        if (isRepos)
+        {
+            if (hit.collider.name == hitGameObjectname)
+            {
+                GameObject.Find(rePosTemplist).transform.GetChild(rePosTempListChildl).gameObject.GetComponent<SpriteRenderer>().material = GameObject.Find(rePosTemplist).transform.GetChild(4).gameObject.GetComponent<SpriteRenderer>().material;
+                GameObject.Find(rePosTemplist).transform.GetChild(rePosTempListChildl).gameObject.tag = GameObject.Find(rePosTemplist).transform.GetChild(4).gameObject.tag;
+                GameObject.Find(rePosTemplist).transform.GetChild(4).gameObject.SetActive(false);
+                GameObject.Find(rePosTemplist).transform.GetChild(rePosTempListChildl).gameObject.SetActive(true);
+                isRepos = false;
+            }
+            else if (hit.collider.name != hitGameObjectname)
+            {
+                string gameObjectHit = hit.collider.name;
+                for (int j = 0; j <= 4; j++)
+                {
+                    if (!GameObject.Find(gameObjectHit).transform.GetChild(j).gameObject.activeInHierarchy)
+                    {
+                        if (j == 0)
+                        {
+                            //Debug.Log(j);
+                            GameObject.Find(rePosTemplist).transform.GetChild(4).gameObject.SetActive(false);
+                            GameObject.Find(gameObjectHit).transform.GetChild(j).gameObject.SetActive(true);
+                            GameObject.Find(gameObjectHit).transform.GetChild(j).gameObject.GetComponent<SpriteRenderer>().material = GameObject.Find(rePosTemplist).transform.GetChild(rePosTempListChildl).gameObject.GetComponent<SpriteRenderer>().material;
+                            GameObject.Find(gameObjectHit).transform.GetChild(j).gameObject.tag = GameObject.Find(rePosTemplist).transform.GetChild(rePosTempListChildl).gameObject.tag;
+                            GameObject.Find(rePosTemplist).transform.GetChild(rePosTempListChildl).gameObject.SetActive(false);
+                            isRepos = false;
+                            break;
+                        }
+                        else if (j == 1)
+                        {
+                            int p = j;
+                            if (GameObject.Find(gameObjectHit).transform.GetChild(p - 1).gameObject.tag == GameObject.Find(rePosTemplist).transform.GetChild(rePosTempListChildl).gameObject.tag)
+                            {
+                                GameObject.Find(rePosTemplist).transform.GetChild(4).gameObject.SetActive(false);
+                                GameObject.Find(gameObjectHit).transform.GetChild(j).gameObject.SetActive(true);
+                                GameObject.Find(gameObjectHit).transform.GetChild(j).gameObject.GetComponent<SpriteRenderer>().material = GameObject.Find(rePosTemplist).transform.GetChild(rePosTempListChildl).gameObject.GetComponent<SpriteRenderer>().material;
+                                GameObject.Find(gameObjectHit).transform.GetChild(j).gameObject.tag = GameObject.Find(rePosTemplist).transform.GetChild(rePosTempListChildl).gameObject.tag;
+                                GameObject.Find(rePosTemplist).transform.GetChild(rePosTempListChildl).gameObject.SetActive(false);
+                                isRepos = false;
+                                break;
+                            }
+                            else
+                            {
+                                GameObject.Find(rePosTemplist).transform.GetChild(rePosTempListChildl).gameObject.GetComponent<SpriteRenderer>().material = GameObject.Find(rePosTemplist).transform.GetChild(4).gameObject.GetComponent<SpriteRenderer>().material;
+                                GameObject.Find(rePosTemplist).transform.GetChild(4).gameObject.SetActive(false);
+                                GameObject.Find(rePosTemplist).transform.GetChild(rePosTempListChildl).gameObject.SetActive(true);
+                                GameObject.Find(rePosTemplist).transform.GetChild(rePosTempListChildl).gameObject.tag = GameObject.Find(rePosTemplist).transform.GetChild(4).gameObject.tag;
+                                isRepos = false;
+                                break;
+                            }
+                        }
+                        else if (j == 2)
+                        {
+                            int p = j;
+                            if (GameObject.Find(gameObjectHit).transform.GetChild(p - 1).gameObject.tag == GameObject.Find(rePosTemplist).transform.GetChild(rePosTempListChildl).gameObject.tag)
+                            {
+                                GameObject.Find(rePosTemplist).transform.GetChild(4).gameObject.SetActive(false);
+                                GameObject.Find(gameObjectHit).transform.GetChild(j).gameObject.SetActive(true);
+                                GameObject.Find(gameObjectHit).transform.GetChild(j).gameObject.GetComponent<SpriteRenderer>().material = GameObject.Find(rePosTemplist).transform.GetChild(rePosTempListChildl).gameObject.GetComponent<SpriteRenderer>().material;
+                                GameObject.Find(gameObjectHit).transform.GetChild(j).gameObject.tag = GameObject.Find(rePosTemplist).transform.GetChild(rePosTempListChildl).gameObject.tag;
+                                GameObject.Find(rePosTemplist).transform.GetChild(rePosTempListChildl).gameObject.SetActive(false);
+                                isRepos = false;
+                                break;
+                            }
+                            else
+                            {
+                                GameObject.Find(rePosTemplist).transform.GetChild(rePosTempListChildl).gameObject.GetComponent<SpriteRenderer>().material = GameObject.Find(rePosTemplist).transform.GetChild(4).gameObject.GetComponent<SpriteRenderer>().material;
+                                GameObject.Find(rePosTemplist).transform.GetChild(4).gameObject.SetActive(false);
+                                GameObject.Find(rePosTemplist).transform.GetChild(rePosTempListChildl).gameObject.SetActive(true);
+                                GameObject.Find(rePosTemplist).transform.GetChild(rePosTempListChildl).gameObject.tag = GameObject.Find(rePosTemplist).transform.GetChild(4).gameObject.tag;
+                                isRepos = false;
+                                break;
+                            }
+                        }
+                        else if (j == 3)
+                        {
+                            int p = j;
+                            if (GameObject.Find(gameObjectHit).transform.GetChild(p - 1).gameObject.tag == GameObject.Find(rePosTemplist).transform.GetChild(rePosTempListChildl).gameObject.tag)
+                            {
+                                GameObject.Find(rePosTemplist).transform.GetChild(4).gameObject.SetActive(false);
+                                GameObject.Find(gameObjectHit).transform.GetChild(j).gameObject.SetActive(true);
+                                GameObject.Find(gameObjectHit).transform.GetChild(j).gameObject.GetComponent<SpriteRenderer>().material = GameObject.Find(rePosTemplist).transform.GetChild(rePosTempListChildl).gameObject.GetComponent<SpriteRenderer>().material;
+                                GameObject.Find(gameObjectHit).transform.GetChild(j).gameObject.tag = GameObject.Find(rePosTemplist).transform.GetChild(rePosTempListChildl).gameObject.tag;
+                                GameObject.Find(rePosTemplist).transform.GetChild(rePosTempListChildl).gameObject.SetActive(false);
+                                isRepos = false;
+                                break;
+                            }
+                            else
+                            {
+                                GameObject.Find(rePosTemplist).transform.GetChild(rePosTempListChildl).gameObject.GetComponent<SpriteRenderer>().material = GameObject.Find(rePosTemplist).transform.GetChild(4).gameObject.GetComponent<SpriteRenderer>().material;
+                                GameObject.Find(rePosTemplist).transform.GetChild(4).gameObject.SetActive(false);
+                                GameObject.Find(rePosTemplist).transform.GetChild(rePosTempListChildl).gameObject.SetActive(true);
+                                GameObject.Find(rePosTemplist).transform.GetChild(rePosTempListChildl).gameObject.tag = GameObject.Find(rePosTemplist).transform.GetChild(4).gameObject.tag;
+                                isRepos = false;
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            GameObject.Find(rePosTemplist).transform.GetChild(rePosTempListChildl).gameObject.GetComponent<SpriteRenderer>().material = GameObject.Find(rePosTemplist).transform.GetChild(4).gameObject.GetComponent<SpriteRenderer>().material;
+                            GameObject.Find(rePosTemplist).transform.GetChild(4).gameObject.SetActive(false);
+                            GameObject.Find(rePosTemplist).transform.GetChild(rePosTempListChildl).gameObject.SetActive(true);
+                            GameObject.Find(rePosTemplist).transform.GetChild(rePosTempListChildl).gameObject.tag = GameObject.Find(rePosTemplist).transform.GetChild(4).gameObject.tag;
+                            isRepos = false;
+                            break;
+                        }
+                    }
+                    
+                }
+            }
+        }
+    }
+}
