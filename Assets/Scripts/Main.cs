f2bdde42de2d8a20e4bf086966829daf65a27b19
Gautam-InTechualSolutions <gautam@intechualsolutions.com>
2020-12-21 19:00:14 +0530

    Some Update for ballsort

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -5,31 +5,175 @@ using UnityEngine;
 public class Main : MonoBehaviour
 {
     public GameObject squareBox;
-    GameObject abc;
     public List<Material> materialList;
     List<GameObject> tempList = new List<GameObject>();
-    int listLength, pos = -2, rePosTempListChildl;
+    int listLength, posx = -2, posy = 0, rePosTempListChildl;
     public List<string> tagName;
     string hitGameObjectname, rePosTemplist;
+
+    int k = 0, l = 0, m = 0, n = 0;
     bool isRepos = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        int randomBox = 3;
-        for (int i = 0; i < randomBox; i++)
+       
+        int randomBox =  Random.Range(3,5);
+        if (randomBox > 4)
         {
-            tempList.Add(Instantiate(squareBox, new Vector2(transform.position.x + pos, transform.position.y), Quaternion.identity));
-            tempList[i].gameObject.name = "Square" + i;
-            listLength = Random.Range(2, 3);
-            for (int j = 0; j < listLength; j++)
+            posy = 2;
+            for (int i = 0; i < randomBox; i++)
             {
-                int materialColor = Random.Range(0, 2);
-                tempList[i].transform.GetChild(j).gameObject.SetActive(true);
-                tempList[i].transform.GetChild(j).gameObject.GetComponent<SpriteRenderer>().material = materialList[materialColor];
-                tempList[i].transform.GetChild(j).gameObject.tag = tagName[materialColor];
+                if (i < 4)
+                {
+                    if (posx > 3)
+                    {
+                        posx = -2;
+                        posy = -2;
+                    }
+                    tempList.Add(Instantiate(squareBox, new Vector2(transform.position.x + posx, transform.position.y + posy), Quaternion.identity));
+                    tempList[i].gameObject.name = "Square" + i;
+                    //listLength = Random.Range(2, 3);
+                    for (int j = 0; j < 4; j++)
+                    {
+                        int materialColor = Random.Range(0, 4);
+                        tempList[i].transform.GetChild(j).gameObject.SetActive(true);
+
+                        switch (materialColor)
+                        {
+                            case 0:
+                                if (materialColor == 0)
+                                {
+                                    if (k < 4)
+                                    {
+                                        k = k + 1;
+                                        Debug.Log(k + " && " + materialColor);
+                                    }
+                                    else
+                                    {
+                                        materialColor = 1;
+                                        goto case 1;
+                                    }
+                                }
+                                break;
+                            case 1:
+                                if (materialColor == 1)
+                                {
+                                    if (l < 4)
+                                    {
+                                        l = l + 1;
+                                        Debug.Log(l + " && " + materialColor);
+                                    }
+                                    else
+                                    {
+                                        materialColor = 2;
+                                        goto case 2;
+                                    }
+                                }
+                                break;
+                            case 2:
+                                if (materialColor == 2)
+                                {
+                                    if (m < 4)
+                                    {
+                                        m = m + 1;
+                                        Debug.Log(m + " && " + materialColor);
+                                    }
+                                    else
+                                    {
+                                        materialColor = 3;
+                                        goto case 3;
+                                    }
+                                }
+                                break;
+                            case 3:
+                                if (materialColor == 3)
+                                {
+                                    if (n < 4)
+                                    {
+                                        n = n + 1;
+                                        Debug.Log(n + " && " + materialColor);
+                                    }
+                                    else
+                                    {
+                                        materialColor = 0;
+                                        goto case 0;
+                                    }
+                                }
+                                break;
+                        }
+                        tempList[i].transform.GetChild(j).gameObject.GetComponent<SpriteRenderer>().material = materialList[materialColor];
+                        tempList[i].transform.GetChild(j).gameObject.tag = tagName[materialColor];
+                    }
+                }
+                else
+                {
+                    tempList.Add(Instantiate(squareBox, new Vector2(transform.position.x + posx, transform.position.y + posy), Quaternion.identity));
+                    tempList[i].gameObject.name = "Square" + i;
+                    tempList.Add(Instantiate(squareBox, new Vector2(transform.position.x + posx + 2, transform.position.y + posy), Quaternion.identity));
+                    tempList[i].gameObject.name = "Square" + i;
+                }
+                posx = posx + 2;
+
             }
-            pos = pos + 2;
+        }
+        else
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (i < 2)
+                {
+                    tempList.Add(Instantiate(squareBox, new Vector2(transform.position.x + posx, transform.position.y + posy), Quaternion.identity));
+                    tempList[i].gameObject.name = "Square" + i;
+                    //listLength = Random.Range(2, 3);
+                    for (int j = 0; j < 4; j++)
+                    {
+                        int materialColor = Random.Range(0, 2);
+                        tempList[i].transform.GetChild(j).gameObject.SetActive(true);
+                        switch (materialColor)
+                        {
+                            case 0:
+                                if (materialColor == 0)
+                                {
+                                    if (k < 4)
+                                    {
+                                        k = k + 1;
+                                        Debug.Log(k + " && " + materialColor);
+                                    }
+                                    else
+                                    {
+                                        materialColor = 1;
+                                        goto case 1;
+                                    }
+                                }
+                                break;
+                            case 1:
+                                if (materialColor == 1)
+                                {
+                                    if (l < 4)
+                                    {
+                                        l = l + 1;
+                                        Debug.Log(l + " && " + materialColor);
+                                    }
+                                    else
+                                    {
+                                        materialColor = 0;
+                                        goto case 0;
+                                    }
+                                }
+                                break;
+                        }
+                        tempList[i].transform.GetChild(j).gameObject.GetComponent<SpriteRenderer>().material = materialList[materialColor];
+                        tempList[i].transform.GetChild(j).gameObject.tag = tagName[materialColor];
+                    }
+                }
+                else
+                {
+                    tempList.Add(Instantiate(squareBox, new Vector2(transform.position.x + posx, transform.position.y + posy), Quaternion.identity));
+                    tempList[i].gameObject.name = "Square" + i;
+                }
+                posx = posx + 2;
+            }
         }
     }
 
@@ -89,7 +233,7 @@ public class Main : MonoBehaviour
                     isRepos = true;
                     break;
                 }
-                
+
             }
         }
     }
@@ -203,7 +347,6 @@ public class Main : MonoBehaviour
                             break;
                         }
                     }
-                    
                 }
             }
         }
